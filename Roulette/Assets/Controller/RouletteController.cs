07180956bfef955a2b34716b1c1a3ea6c9c2d99e
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2019-07-19 00:48:52 +0900

    first commit

---
+++ Roulette/Assets/Controller/RouletteController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+  float rotateSpeed = 0;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButton(0))
+    {
+      this.rotateSpeed = 10;
+    }
+
+    transform.Rotate(0, 0, this.rotateSpeed);
+
+    this.SetNewRotateSpeed();
+  }
+
+  void SetNewRotateSpeed()
+  {
+    this.rotateSpeed *= 0.96f;
+  }
+}
